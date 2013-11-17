a795d96c9614177c42be9804c7b62d8ee1031ba2
rafaeldsmoreira <rafael.dsmoreira@gmail.com>
2013-11-17 13:52:24 -0200

    Finist

--- js/controllers/ContasController.js
+++ js/controllers/ContasController.js
@@ -1,12 +1,17 @@
 conversionApp.controller('ContasController', function($scope) {
 
 	//initialization	
-	$scope.entradas = true;
+	$scope.relatorios = true;
 	$scope.contas=[];
-	$scope.pgcontas=[];
+	$scope.contasApg=[];
+	
 	$scope.STORAGE_KEY=$scope.conta;
-	$scope.STORAGEPGCONTA_KEY=$scope.pgcontas;
+	$scope.STORAGEPGCONTA_KEY=$scope.pgconta;
 	$scope.valorTotal='';
+	$scope.flag='';
+	$scope.ContaAsPgfalse
+
+
 
 
 //----------------------------------------------------------------------------------------------------
@@ -18,10 +23,12 @@ conversionApp.controller('ContasController', function($scope) {
 		        $scope.salveStorageContas();
 				$scope.reset();
 				$scope.load();
+				$scope.setrelPagarconta();
 			}else{
 				$scope.salveStorageContas();
 				$scope.reset();
 				$scope.load();
+				$scope.setrelPagarconta();
 
 			}
     };
@@ -30,18 +37,12 @@ conversionApp.controller('ContasController', function($scope) {
 
 $scope.addContasPg= function(){
 
-		if(!$scope.pgconta.idConta){
 
-				$scope.addListContas();
-		        salveStorageContas();
-				$scope.reset();
-				$scope.load();
-			}else{
-				salveStorageContas();
-				$scope.reset();
-				$scope.load();
-
-			}
+				$scope.removeConta($scope.ContaAsPg);
+				$scope.addListContasPg();
+		        $scope.salveStorageContasPg();
+		        $scope.setrelatorio();
+			
     };
 	
 
@@ -70,10 +71,8 @@ $scope.addContasPg= function(){
 		$scope.conta=contaAseralterada;
 		$scope.index = $scope.contas.indexOf(contaAseralterada);
 		$scope.contas[index] = $scope.conta;
+		
 	};
-
-
-
 
 
 //-------------------------------------------------------------------------------------------------------	
@@ -81,36 +80,40 @@ $scope.addContasPg= function(){
 
 		$scope.setpagar();
 		$scope.pgconta=contaApg;
-		
+		$scope.pgconta.datalancamento=$scope.dataAtual();
+		$scope.ContaAsPg=contaApg;
 
-		//$scope.pgconta.valor=$scope.pgconta.multa+$scope.pgconta.valor;
-		
+
+
+			
 
     };
+    $scope.pagarContafinalizar=function(contaApg) {
+
+		$scope.removeConta($scope.indexConta);
+		$scope.contas.splice($scope.indexConta,1);
+		
+	};
+
 
 
     $scope.calcularTotal=function() {
      
 		
-		 $socpe.valtemp='';
-		 $socpe.valtempMulta='';
-		 $socpe.valtemp=$scope.pgconta.valor;
-		 $socpe.valtempMulta=$scope.pgconta.Multa;
-		 $scope.pgconta.valor='';
-         $scope.pgconta.valor=$socpe.valtempMulta+$socpe.valtemp;
-         
+		 $scope.valtemp='';
+		 $scope.valtempMulta='';
+		 $scope.valtemp=parseFloat($scope.pgconta.valor);
+		 $scope.valtempMulta=parseFloat($scope.pgconta.multa);
+		 $scope.valtempJuros=parseFloat($scope.pgconta.jurosaodia);
+ 		if(!$scope.pgconta.multa){$scope.valtempMulta=0; }
 
+		 if(!$scope.pgconta.jurosaodia){$scope.juros=0; 
+		  	}else
+		  	 $scope.juros=parseFloat(($scope.valtemp*$scope.valtempJuros/100));
+		$scope.pgconta.valorApg='';
+		$scope.pgconta.valorApg=($scope.valtempMulta+$scope.valtemp+$scope.juros);
 
-
-
-
-    };
-
-
-
-
-
-
+       };
 
 //-------------------------------------------------------------------------------------------------------------------
 	$scope.addListContas= function() {
@@ -119,7 +122,8 @@ $scope.addContasPg= function(){
 		idEntrada:$scope.generateId(), 
 		descricao:$scope.conta.descricao,
 		vencimento:$scope.conta.vencimento,
-		valor:$scope.conta.valor
+		valor:$scope.conta.valor,
+		status:'Np'
 					
 		});
 	};
@@ -127,39 +131,50 @@ $scope.addContasPg= function(){
 
 
 		$scope.addListContasPg= function() {
-		$scope.contaspg.push({
-		idConta:$scope.generateId,	
-		descricaocpg:$scope.pgconta.descricaocpg,
-		datalancamento:$scope.pgconta.datalancamento,
-		datavencimento:$scope.pgconta.datavencimento,
+		
+		
+		$scope.contasApg.push({
+		
+		
+		idConta:$scope.generateId(), 
+		descricao:$scope.pgconta.descricao,
+		lancamento:$scope.pgconta.datalancamento,
+		vencimento:$scope.pgconta.vencimento,
 		multa:$scope.pgconta.multa,
 		jurosaodia:$scope.pgconta.jurosaodia,
-		valorConta:$scope.pgconta.valorConta
-					
+		valor:$scope.pgconta.valor,
+		valorApg:$scope.pgconta.valorApg,
+		status:'Pg'
+		
+						
 		});
 	};
 
-
+//--------------------------------------------------------------------------------------------------
 
     $scope.salveStorageContas = function() {
-		localStorage.setItem($scope.STORAGE_KEY, JSON.stringify($scope.contas));
+		localStorage.setItem("Contas", JSON.stringify($scope.contas));
 		
+};
+
+    $scope.salveStorageContasPg = function() {
+		localStorage.setItem("ContasPg", JSON.stringify($scope.contasApg));
 		
-	};
+};
 
 
-	 $scope.salveStorageContasPg = function() {
-		localStorage.setItem($scope.STORAGEPGCONTA_KEY, JSON.stringify($scope.pgcontas));
-		
-		
-	};
+
 
 
 
 //--------------------------------------------------------------------------------------------------------------------	
     $scope.load=function() {
-		var ContasStr = localStorage.getItem($scope.STORAGE_KEY);
+		var ContasStr = localStorage.getItem("Contas");
+
+		
 		$scope.contas = JSON.parse(ContasStr);
+		$scope.loadPg();
+		
 		if($scope.contas) {
 			return true;
 		}
@@ -167,33 +182,91 @@ $scope.addContasPg= function(){
 		return false;
     };
 
-	//--------------------------------------------------------------------------------------------------------------------	
-
-
-	$scope.update= function(conta) {
-		var index = $scope.contas.indexOf(conta);
-					
-		if(index > -1) {
-			$scope.contas[index] = $scope.conta;
+    $scope.loadPg=function() {
+		var ContasStrpg = localStorage.getItem("ContasPg");
+		
+		
+		$scope.contasApg = JSON.parse(ContasStrpg);
+		
+		if($scope.contasApg) {
 			return true;
 		}
+		$scope.contasApg = [];
 		return false;
-	};
-//---------------------------------------------------------------------------------------------------------
+    };
+
+      
+
+
 
 
 //---------------------------------------------------------------------------------------------------------
 
 	$scope.removeConta=function(conta) {
-		alert("exclui:"+conta.descricao);
-		var index = $scope.contas.indexOf(conta);
 		
+		
+		var index = $scope.contas.indexOf(conta);
+
+			
 		if(index > -1) {
 			$scope.contas.splice(index,1);
+			localStorage.removeItem("Contas");
+			$scope.salveStorageContas();
 			return true;
+			
 		}
+
+
+			
 		
 		return false;
+
+		
+	};
+
+
+
+
+	$scope.estornarContaPg=function(conta) {
+		
+		
+		var index = $scope.contasApg.indexOf(conta);
+		$scope.conta=conta;
+		$scope.addContas();
+			
+		if(index > -1) {
+			$scope.contasApg.splice(index,1);
+			localStorage.removeItem("ContasPg");
+			$scope.salveStorageContasPg();
+			return true;
+			
+		}
+
+
+			
+		
+		return false;
+
+		
+	};
+
+
+
+	$scope.dataAtual=function(){
+
+
+
+		hoje = new Date()
+dia = hoje.getDate()
+mes = hoje.getMonth()
+ano = hoje.getFullYear()
+if (dia < 10)
+dia = "0" + dia
+if (ano < 2000)
+ano = "19" + ano
+
+//O mes começa em Zero, então soma-se 1
+return(dia+"/"+(mes+1)+"/"+ano)
 	};
 
 
@@ -213,6 +286,8 @@ $scope.addContasPg= function(){
 		$scope.relatorios =  false;
 		$scope.entradas =  true;
 		 $scope.pagar =  false;
+		 $scope.relatoriospagas=false;
+		 $scope.contasPg=false;
 	};
 //--------------------------------------------------------------------------------------------------------------------		
 
@@ -220,6 +295,19 @@ $scope.addContasPg= function(){
 		$scope.relatorios =  false;
 		$scope.entradas =  false;
 		$scope.pagar =  true;
+		$scope.contasPg=false;
+		$scope.relContasApagar=false;
+		
+	};
+
+
+	$scope.setrelPagarconta= function() {
+		$scope.relContasApagar=true;
+		$scope.relatorios =  false;
+		$scope.entradas =  false;
+		$scope.pagar =  false;
+		$scope.contasPg=false;
+		
 	};
 //------------------------------------------------------------------------------------------------------------------------
 
@@ -227,9 +315,19 @@ $scope.addContasPg= function(){
 		$scope.relatorios =  true;
 		$scope.entradas =  false;
 		$scope.pagar =  false;
+		$scope.contasPg=false;
+		$scope.relContasApagar=false;
+
 	};
 
 	
+	$scope.setContasPagas= function() {
+		$scope.relatorios =  false;
+		$scope.entradas =  false;
+		$scope.pagar =  false;
+		$scope.contasPg=true;
+		$scope.relContasApagar=false;
+	};
 
 	
 
